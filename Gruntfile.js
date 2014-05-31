80b1b1e06291bc44f7a9b4275f837591c5f10754
Maurizio Mangione <granzebru@gmail.com>
2014-05-31 23:30:33 +0200

    demo files added

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,46 @@ module.exports = function (grunt) {
   'use strict';
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      dist: ['js/*.js', '!js/all.js'],
+      options: {
+        force: true,
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'css/main.css': 'sass/main.scss'
+        }
+      }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 1 version', 'ie 8']
+      },
+      all: {
+        src: ['css/main.css']
+      }
+    },
+    watch: {
+      sass: {
+        files: 'sass/*.scss',
+        tasks: ['sass', 'autoprefixer']
+      },
+      jsHint: {
+        files: ['Gruntfile.js', 'js/*.js', '!js/all.js'],
+        tasks: ['jshint']
+      }
+    }
   });
 
   require('load-grunt-tasks')(grunt);
 
-
-  grunt.registerTask('default', function () {
-    var tasks = [];
+  grunt.registerTask('default', function(){
+    var tasks = ['jshint', 'sass', 'autoprefixer', 'watch'];
 
     grunt.option('force', true);
     grunt.task.run(tasks);
---
+++ js/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+(function sayHello () {
+  console.log('Hello world!');
+})();
