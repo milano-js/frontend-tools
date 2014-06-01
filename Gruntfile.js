a762cab42cba1a3b67be46e29e1651d9d1c06269
Maurizio Mangione <granzebru@gmail.com>
2014-06-01 22:39:24 +0200

    step 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,38 +10,13 @@ module.exports = function (grunt) {
         jshintrc: '.jshintrc',
         reporter: require('jshint-stylish')
       }
-    },
-    sass: {
-      dist: {
-        files: {
-          'css/main.css': 'sass/main.scss'
-        }
-      }
-    },
-    autoprefixer: {
-      options: {
-        browsers: ['last 1 version', 'ie 8']
-      },
-      all: {
-        src: ['css/main.css']
-      }
-    },
-    watch: {
-      sass: {
-        files: 'sass/*.scss',
-        tasks: ['sass', 'autoprefixer']
-      },
-      jsHint: {
-        files: ['Gruntfile.js', 'js/*.js', '!js/all.js'],
-        tasks: ['jshint']
-      }
     }
   });
 
   require('load-grunt-tasks')(grunt);
 
   grunt.registerTask('default', function(){
-    var tasks = ['jshint', 'sass', 'autoprefixer', 'watch'];
+    var tasks = ['jshint'];
 
     grunt.option('force', true);
     grunt.task.run(tasks);
